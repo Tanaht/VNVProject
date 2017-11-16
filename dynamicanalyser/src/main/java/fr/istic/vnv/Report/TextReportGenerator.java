5520aedb75276fa83dc319bb43e72aff74f034b1
Tanaky <charp.antoine@gmail.com>
2017-11-16 11:41:31 +0100

    Indent Execution Trace reports

--- dynamicanalyser/src/main/java/fr/istic/vnv/Report/TextReportGenerator.java
+++ dynamicanalyser/src/main/java/fr/istic/vnv/Report/TextReportGenerator.java
@@ -1,6 +1,7 @@
 package fr.istic.vnv.Report;
 
 import java.io.PrintStream;
+import java.util.List;
 
 public class TextReportGenerator extends ReportGenerator {
 
@@ -9,8 +10,23 @@ public class TextReportGenerator extends ReportGenerator {
         stream.println("Execution Trace:");
         stream.println("==================");
 
-        for(String trace : super.getContext().getExecutionTrace()) {
-            stream.println(trace);
+        stream.println(printExecutionTrace("", super.getContext().getExecutionTrace()));
+    }
+
+    private String printExecutionTrace(String tab, List<String> trace) {
+        if(trace.size() == 0)
+            return "";
+
+        String current = trace.get(0);
+
+
+        if(current.startsWith("[START]")) {
+            return tab + current + '\n' + printExecutionTrace(tab + '\t', trace.subList(1, trace.size()));
+        } else if(current.startsWith("[END]")) {
+            tab = tab.substring(1);
+            return tab + current + '\n' + printExecutionTrace(tab, trace.subList(1, trace.size()));
         }
+
+        throw new RuntimeException("Should never be executed");
     }
 }
