efb1f8eeffc5bacf7fd44af60c3a9188abb5ff63
Unknown <beaulielloic@gmail.com>
2017-12-22 14:51:34 +0100

    add test for ReportGeneratorFactory

--- dynamicanalyser/src/test/java/fr/istic/vnv/report/TestReportGeneratorFactory.java
+++ dynamicanalyser/src/test/java/fr/istic/vnv/report/TestReportGeneratorFactory.java
@@ -1,6 +1,19 @@
 package fr.istic.vnv.report;
 
-public class TestReportGeneratorFactory {
+import fr.istic.vnv.analysis.ClassContext;
+import org.junit.jupiter.api.Test;
 
+import java.io.File;
+import java.io.IOException;
+
+import static org.hamcrest.CoreMatchers.any;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.mockito.Matchers.anyString;
+
+public class TestReportGeneratorFactory {
+    @Test
+    void getTextReportGenerator_can_not_return_null() throws IOException {
+        assertThat(ReportGeneratorFactory.getTextReportGenerator(new File("src")),any(ReportGenerator.class));
+    }
 
 }
